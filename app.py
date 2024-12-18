a1ea8207f6a1d92bafa4eb88ca8eb877e7fa2182
Hritik827 <78196827+Hritik827@users.noreply.github.com>
2024-12-17 18:36:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ def addition(a,b,c):
     return a+b+c
 
 
-<<<<<<< HEAD
-def substraction(a,b,e):
-    return a-b-e
-=======
-def substraction(a,b,c):
-    return a-b-c
->>>>>>> 1834c4e97a39ccd945c80ff75ab3533e107fa7a1
+
+def substraction(a,b,d):
+    return a-b-d
+
 
 
 def division(a,b):
